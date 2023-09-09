1f585d6b6aad1115265910ab9800990a4335d56a
EltonBata <eltonbata14@gmail.com>
2023-09-09 22:36:58 +0200

    feat: add and configure express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world")
+const express = require("express");
+const app = express();
+import {config } from "dotenv";
+
+config();
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => { console.log(`listening on port ${port}`) })
