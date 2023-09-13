41981593a3e26ac03952c971089fb31a2b8482c2
EltonBata <eltonbata14@gmail.com>
2023-09-13 09:37:48 +0200

    refactor: add http status code helper

--- src/responses/HttpResponse.ts
+++ src/responses/HttpResponse.ts
@@ -1,27 +1,28 @@
 import exp from "constants";
+import { HttpStatusCode } from "../helpers/HttpStatusCode";
 
 export interface HttpResponse<T> {
-  statusCode: number;
+  statusCode: HttpStatusCode;
   body: T;
 }
 
 export const done = <T>(data: T): HttpResponse<T> => ({
-  statusCode: 200,
+  statusCode: HttpStatusCode.OK,
   body: data,
 });
 
 export const created = <T>(data: T): HttpResponse<T> => ({
-  statusCode: 201,
+  statusCode: HttpStatusCode.CREATED,
   body: data,
 });
 
 export const badRequest = (message: string): HttpResponse<string> => ({
-  statusCode: 400,
+  statusCode: HttpStatusCode.BAD_REQUEST,
   body: message,
 });
 
 export const serverError = (): HttpResponse<string> => ({
-  statusCode: 500,
+  statusCode: HttpStatusCode.SERVER_ERROR,
   body: "Something went wrong",
 });
 
